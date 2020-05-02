6d6a4f4581d0c040c70eed911c64aaedde3195ca
alex <sashabyglak80@gmali.com>
2020-05-02 22:40:39 +0300

    lab 2

--- Lab_1/LINUX/main.cpp
+++ Lab_1/LINUX/main.cpp
@@ -5,16 +5,15 @@ using namespace std;
 
 int main(int argc, char **argv) {
     pid_t pid = fork();
-
-    switch (pid) {
-        case -1:
-            break;
-        case 0:
-            //todo: execl()
-            break;
-        default:
-            //todo: close child
-
-            break;
-    }
+    cout << "lab1" << endl;
+//    switch (pid) {
+//        case -1:
+//            break;
+//        case 0:
+//            //todo: execl()
+//            break;
+//        default:
+//            //todo: close child
+//            break;
+//    }
 }
